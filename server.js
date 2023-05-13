377b7c02b84a55f1df898c9facc3b9dd39472488
MariNoWool <115777787+MariNoWool@users.noreply.github.com>
2023-05-13 12:25:37 +0200

    Initial author routes

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 //Establish route to file index.js
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('puclic'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -24,5 +27,6 @@ db.once('error', error => console.log("Connected to Mongoose"))
 
 //Use route established in indexRouter whenever you are in the root of the program
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
